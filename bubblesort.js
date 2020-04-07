cc3e1ee2307c671a4e3db05f9716a4e4b3430dd7
Shannon <scrowley016@gmail.com>
2020-04-07 12:37:29 -0400

    bubble sort working on test array

--- bubblesort.js
+++ bubblesort.js
@@ -1,27 +1,21 @@
 function bubbleSort(array){
-    let sortArr= []
-    for(i=0;i< array.length;i++){
-        let ele1=array[i]
-        let ele2=array[i+1]
-        if (i === array.length-1){
-            sortArr.unshift(ele1)
-            let newArray = array.slice(-1)
-            bubbleSort(newArray)
-        }else{
-            if(ele1<ele2){
-                continue
-            }
-            else if(ele1>ele2){
-                swapElement(array,i,i+1)
-            }
-        }
-    }
-    return sortArr
-}
-
-function swapElement(array,index1,index2){
-    var memo = array[index1]
-    array[index1]= array[index2]
-    array[index2]= memo
-    return array
-}
+    for(let i=0;i< array.length;i++){
+        for(let j=0;j<(array.length);j++){
+              if(array[j]>array[j+1]){
+                  swapElement(array,j,j+1)
+              }
+        }  
+      }return array
+  }
+  
+  function swapElement(array,index1,index2){
+      var memo = array[index1]
+      array[index1]= array[index2]
+      array[index2]= memo
+      return array
+  }
+  
+  
+  let arr = [3, 7, 19, 4, 2, 9,16,11]
+  
+  bubbleSort(arr)
